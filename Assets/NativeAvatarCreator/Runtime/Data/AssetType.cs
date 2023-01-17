322978dc42e9f4fe2e5e2fa74b53aebeb8c7d513
Robin <1121080+rYuuk@users.noreply.github.com>
2023-01-17 21:47:17 +0100

    chore: extra AssetType to class

---
+++ Assets/NativeAvatarCreator/Runtime/Data/AssetType.cs
@@ -0,0 +1,24 @@
+﻿namespace NativeAvatarCreator
+{
+    public static class AssetType
+    {
+        public static readonly string[] Face = {
+            "faceshape",
+            "eyeshape",
+            "eye",
+            "eyebrows",
+            "noseshape",
+            "lipshape",
+            "beard",
+        };
+
+        public static readonly string[] Body = {
+            "hair",
+            "outfit",
+            "glasses",
+            "facemask",
+            "facewear",
+            "headwear"
+        };
+    }
+}
--- Assets/NativeAvatarCreator/Runtime/Data/Payload.cs
+++ Assets/NativeAvatarCreator/Runtime/Data/Payload.cs
@@ -1,27 +1,6 @@
 ﻿namespace NativeAvatarCreator
 {
-    public static class AssetType
-    {
-        public static readonly string[] Face = {
-            "faceshape",
-            "eyeshape",
-            "eye",
-            "eyebrows",
-            "noseshape",
-            "lipshape",
-            "beard",
-        };
 
-        public static readonly string[] Body = {
-            "hair",
-            "outfit",
-            "glasses",
-            "facemask",
-            "facewear",
-            "headwear"
-        };
-    }
-    
     public class Payload
     {
         // Required Fields
