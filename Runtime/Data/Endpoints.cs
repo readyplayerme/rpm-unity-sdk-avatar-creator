bf7670c9dd107a6a19c175f4ad84d889f991856e
Robin <1121080+rYuuk@users.noreply.github.com>
2023-03-23 13:21:17 +0100

    chore: switch to production api

--- Runtime/Data/Endpoints.cs
+++ Runtime/Data/Endpoints.cs
@@ -6,7 +6,7 @@
         public const string AUTH = DOMAIN_URL + "/api/users";
         public const string ASSETS = DOMAIN_URL + "/api/assets";
 
-        public const string AVATAR_API_V1 = "https://develop.api.readyplayer.me/v1/avatars";
-        public const string AVATAR_API_V2 = "https://develop.api.readyplayer.me/v2/avatars";
+        public const string AVATAR_API_V1 = "https://api.readyplayer.me/v1/avatars";
+        public const string AVATAR_API_V2 = "https://api.readyplayer.me/v2/avatars";
     }
 }
