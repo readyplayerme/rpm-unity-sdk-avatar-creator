982fd2fe61aeda0f6921ea1bd2cf2de48e89c615
Robin <1121080+rYuuk@users.noreply.github.com>
2023-05-23 12:58:52 +0200

    fix: nerf regex for email

--- Samples~/Scripts/Utils/ValidatorUtil.cs
+++ Samples~/Scripts/Utils/ValidatorUtil.cs
@@ -4,9 +4,7 @@ namespace ReadyPlayerMe
 {
     public static class ValidatorUtil
     {
-        private const string EMAIL_REGEX = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
-                                           + "@"
-                                           + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+        private const string EMAIL_REGEX = @".+\@.+\..+";
 
         public static bool IsValidEmail(string email)
         {
