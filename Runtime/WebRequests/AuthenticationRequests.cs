bd697b6c76273944dedbb6299748d370e10a9167
Robin <1121080+rYuuk@users.noreply.github.com>
2023-05-23 14:45:07 +0200

    feat: extract constants to a class

--- Runtime/WebRequests/AuthenticationRequests.cs
+++ Runtime/WebRequests/AuthenticationRequests.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using ReadyPlayerMe.Core;
-using UnityEngine;
 
 namespace ReadyPlayerMe.AvatarCreator
 {
@@ -31,86 +28,70 @@ namespace ReadyPlayerMe.AvatarCreator
             var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers);
             response.ThrowIfError();
 
-            var data = ParseResponse(response.Text);
+            var data = AuthDataConverter.ParseResponse(response.Text);
             return JsonConvert.DeserializeObject<UserSession>(data!.ToString());
         }
 
         public async Task SendCodeToEmail(string email)
         {
             var url = GetUrl(Endpoints.AUTH_START);
-            var payload = new JObject(new JProperty("data",
-                new JObject(
-                    new JProperty("email", email),
-                    new JProperty("authType", "code")
-                )
-            ));
-            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload.ToString());
+            var payload = AuthDataConverter.CreatePayload(new Dictionary<string, string>
+            {   
+                { AuthConstants.EMAIL, email },
+                { AuthConstants.AUTH_TYPE, AuthConstants.CODE }
+            });
+
+            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload);
             response.ThrowIfError();
         }
-
+    
         public async Task<UserSession> LoginWithCode(string code)
         {
             var url = GetUrl(Endpoints.AUTH_LOGIN);
-            var payload = new JObject(new JProperty("data",
-                new JObject(
-                    new JProperty("code", code)
-                )
-            ));
+            var payload = AuthDataConverter.CreatePayload(new Dictionary<string, string>
+            {   
+                { AuthConstants.CODE, code }
+            });
 
-            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload.ToString());
+            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload);
             response.ThrowIfError();
 
-            var data = ParseResponse(response.Text);
+            var data = AuthDataConverter.ParseResponse(response.Text);
             return JsonConvert.DeserializeObject<UserSession>(data!.ToString());
         }
         
         public async Task Signup(string email)
         {
             var url = GetUrl(Endpoints.AUTH_SIGNUP);
-            var payload = new JObject(new JProperty("data",
-                new JObject(
-                    new JProperty("email", email)
-                )
-            ));
-            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload.ToString());
+            var payload = AuthDataConverter.CreatePayload(new Dictionary<string, string>
+            {   
+                { AuthConstants.EMAIL, email }
+            });
+            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload);
             response.ThrowIfError();
         }
 
         public async Task<(string,string)> RefreshToken(string token, string refreshToken)
         {
             var url = GetUrl(Endpoints.AUTH_REFRESH);
-            var payload = new JObject(new JProperty("data",
-                new JObject(
-                    new JProperty("token", token),
-                    new JProperty("refreshToken", refreshToken)
-                )
-            ));
+            var payload = AuthDataConverter.CreatePayload(new Dictionary<string, string>
+            {   
+                { AuthConstants.TOKEN, token },
+                { AuthConstants.REFRESH_TOKEN, refreshToken }
+            });
 
-            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload.ToString());
+            var response = await webRequestDispatcher.SendRequest<Response>(url, HttpMethod.POST, headers, payload);
             response.ThrowIfError();
 
-            var data = ParseResponse(response.Text);
-            var newToken = data["token"]!.ToString();
-            var newRefreshToken = data["refreshToken"]!.ToString();
+            var data = AuthDataConverter.ParseResponse(response.Text);
+            var newToken = data[AuthConstants.TOKEN]!.ToString();
+            var newRefreshToken = data[AuthConstants.REFRESH_TOKEN]!.ToString();
             return (newToken, newRefreshToken);
         }
 
         private string GetUrl(string endpoint)
         {
             return endpoint.Replace("[domain]", domain);
-        }
-
-        private JToken ParseResponse(string response)
-        {
-            var json = JObject.Parse(response);
-            var data = json.GetValue("data");
-
-            if (data == null)
-            {
-                throw new Exception("No data received");
-            }
-
-            return data;
         }
     }
 }
