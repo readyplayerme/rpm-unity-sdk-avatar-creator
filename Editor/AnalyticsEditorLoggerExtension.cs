631ee0454f43c68324f4056de9971bf84ab66f2c
Robin <1121080+rYuuk@users.noreply.github.com>
2023-08-31 10:43:27 +0200

    [SDK-378] Add analytics (#44)
    ## [SDK-378](https://ready-player-me.atlassian.net/browse/SDK-378)
    
    ## Description
    
    - Added analytics for avatar creator importing and sample running
    - Made fetching assets json request asynchronous

---
+++ Editor/AnalyticsEditorLoggerExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ReadyPlayerMe.Core.Analytics;
+
+namespace ReadyPlayerMe.AvatarCreator.Editor
+{
+    public static class AnalyticsEditorLoggerExtension
+    {
+        private const string AVATAR_CREATOR_IMPORTED = "avatar creator present in project";
+        private const string AVATAR_CREATOR_VERSION = "com.readyplayerme.avatarcreator";
+
+        public static void LogAvatarCreatorImported(this IAnalyticsEditorLogger _, string packageVersion)
+        {
+            if (!AnalyticsEditorLogger.IsEnabled) return;
+            AmplitudeEventLogger.LogEvent(AVATAR_CREATOR_IMPORTED, null, new Dictionary<string, object>
+            {
+                { AVATAR_CREATOR_VERSION, packageVersion }
+            });
+        }
+    }
+}
